8d0098cf5358015befa15974bb02179752e7c945
dc7875 <durgeshchavan1994@gmail.com>
2022-11-24 20:33:38 +0530

    Added: random.sh in Generate_Random_Number Branch

---
+++ random.sh
@@ -0,0 +1,15 @@
+#Problem -> How to Generate Random Numbers using Shell Script ?
+
+
+#!/bin/bash
+
+#num1=$((RANDOM%5)); #(0-4)
+#echo $num1;
+
+#num2=$((RANDOM%5+1)); #(1-5)
+#echo $num2;
+
+#Single Digit No:-
+num3=$((RANDOM%9+1)); #(1-9)
+echo $num3;
+
